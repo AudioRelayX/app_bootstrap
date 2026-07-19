3fda4f6eecc3e1b69363204595ff5b4bbf9322dc
HumanBot000 <tobias.sarosi@gmail.com>
2026-07-19 12:38:44 +0200

    changed package layout

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/widgets.dart';
 
+export 'src/logging.dart';
+export 'src/style.dart';
+
 Future<void> bootstrap({
   required Widget app,
   required Future<void> Function() initializeServices,
